06eb5f6c4df679ae2da7ec3bf52702703e382abf
dm13450 <deanmarkwick@Deans-MacBook-Pro.local>
2014-09-25 13:29:22 +0100

    Initial Cancer First Version
    First working version, with Dean’s algorithm.

--- Initial/Canc.cpp
+++ Initial/Canc.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 //########### Define Constants ###########
 static const double N = 10; 
-static const double iter = 15;
+static const double iter = 10;
 static const double u1 = 0.1;
 static const double u2 = 0.1;
 double r[3] = {1,2,1}; 
@@ -32,9 +32,9 @@ uniform_real_distribution<double> dist(0,1);
 
 
 void gen_trans_rate(vector<double> &transr) { 
-	cout << "Generating rates...." << endl; 
+	//cout << "Generating rates...." << endl; 
 	double rbar = ( r[0]*n[0] + r[1]*n[1] + r[2]*n[2] ) / N;
-	cout << "rbar: " << rbar << endl; 
+	//cout << "rbar: " << rbar << endl; 
 	transr[0] = ( (1-u1)*r[0]*n[0]*n[1] ) / (N * rbar);
 	transr[1] = ( (1-u1)*r[0]*n[0]*n[2] ) / (N * rbar);
 	transr[2] = ( ( u1*r[0]*n[0]/N ) + ( (1-u2)*r[1]*n[1]/N ) ) * n[0] / rbar;
@@ -59,13 +59,13 @@ int main() {
 		double a0 = 0, delta = 0, s1 = 0, s2 = 0; 
 		int mu = 0; 
  
- 		cout << "About to gen rates..." << endl; 
+ 		//cout << "About to gen rates..." << endl; 
 		gen_trans_rate(tr); 
  
 		r1 = dist(mt);
 		r2 = dist(mt);
 
-		cout << "Random Numbers: " << r1 << ", " << r2 << endl; 
+		//cout << "Random Numbers: " << r1 << ", " << r2 << endl; 
 		
 		for(vector<double>::iterator it = tr.begin() ; it != tr.end(); ++it) { 
 			a0 += (*it); 
@@ -73,23 +73,53 @@ int main() {
 
 		delta = (1/a0)*log(1/r1); 
 
-		for(int k = 1; k <= 6 ; k++) { 
 
-			for(int m = 1; m <= (k-1); m++) { 
-				s1 += tr[m-1]; 
-			}
 
-			for(int l = 1; l <= (k); l++) { 
-				s2 += tr[l-1]; 
-			}
+	//Dean's Algorithm for finding mu
+	cout << "Entering Dean's Algo" << endl; 
+	double target = r2*a0 ;
+	int mu_dean = 6 ;
+	while (mu_dean != 0) {
 
-			if(s1 < (r2*a0) && (r2*a0) <= s2) { 
-				mu = k; 
-				cout << "Mu has been set to " << mu << endl;
+	double big_sum = 0;
+	double small_sum = 0;
+		//sum the array 
+
+		for (int k = 0; k < mu_dean; k++) {
+
+			big_sum += tr[k];
+
+		}
+
+		for (int k = 0; k < mu_dean-1; k++) {
+
+			small_sum += tr[k];
+
+		}
+
+
+		//small_sum = big_sum - tr[mu] ; 
+
+		cout << small_sum << " " << target << " " << big_sum << endl;
+
+		bool big_sum_test = target <= big_sum ;
+		bool small_sum_test = target > small_sum ;
+
+		if (big_sum_test) {
+			if (small_sum_test) {
+				cout << "Dean's mu: " << mu_dean << endl;
+				mu = mu_dean;
+				break;
 			}
 		}
 
-		t += delta; 
+		mu_dean--;
+	}
+
+	cout << "Exciting  Dean's Algo" << endl; 
+	//Dean's algorithm end
+	mu = mu_dean;	
+	t += delta; 
 
 		switch(mu) { 
 			case 1: 
@@ -112,6 +142,9 @@ int main() {
 				break;
 			default: 
 				break;
+		}
+		if (n[0]*n[1]*n[2] < 0 ) {
+			return 0;
 		}
 		cout << "Population: " << n[0] << ", " << n[1] << ", " << n[2] << endl; 
 	}
