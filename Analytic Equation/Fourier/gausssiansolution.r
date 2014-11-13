32bf266401f858a79c09a3c820eebad6af886fce
dm13450 <deanmarkwick@Deans-MacBook-Pro.local>
2014-11-13 16:23:23 +0000

    FourierGraphs

--- Analytic Equation/Fourier/gausssiansolution.r
+++ Analytic Equation/Fourier/gausssiansolution.r
@@ -12,34 +12,16 @@ gaussol <- function(x, t){
 
 }
 
-gaussol_v2 <- function(x,t){
+gaussol_v2 <- function(x,t, deltaX, u, x0){
 
-	deltaX = 10 
-	u = 0.1 
+	#deltaX = 10 
+	#u = 0.1 
 	u_tilde = deltaX * u 
-	x0 = 0 
+	#x0 = 0 
 	
 	rho = ( (2*pi) / (deltaX * u_tilde * t)) ^(0.5) * exp( - (u_tilde * t - x + x0)^2 / (2 * deltaX * u_tilde * t  ))
 
 	return(rho)
-
-}
-
-
-
-x = c(1:100)
-
-t = 1 
-
-T = 1000
-
-while(t < T){
-
-	plot(x, gaussol_v2(x,t), type = "l")
-	matplot(x, gaussol(x,t), type="l", add = T, col="Red")
-
-	t = t + 1
-
 
 }
 
