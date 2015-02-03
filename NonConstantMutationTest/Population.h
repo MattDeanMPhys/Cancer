8721858a913064a2d78b606cf5290fb5151e5c85
dm13450 <deanmarkwick@Deans-MacBook-Pro.local>
2015-02-03 14:18:49 +0000

    Continuing with non flat mutations

---
+++ NonConstantMutationTest/Population.h
@@ -0,0 +1,38 @@
+#ifndef POPULATION_H
+#define POPULATION_H
+#include <vector>
+
+class Population {
+
+	int number_of_mutations;
+	int total_number_of_cells;
+	
+	std::vector<int> cells;
+	std::vector<double> fitness;
+	std::vector<double> mutation;
+
+	public:
+
+		Population(int, int, double, double);
+		void Print_Population();
+		void Cell_Death(int);
+		void Cell_Birth(int); 
+		std::vector<int> Get_Population(); 
+		void Update( std::vector<int>); 
+		bool Fixation_Test();
+
+		void Generate_FitnessLandscape(double);
+		void Modify_FitnessLandscape(double, int, int);
+
+		void Generate_MutationLandscape(double);
+		void Modify_MutationLandscape(double, int, int);
+
+		std::vector<double> Get_MutationLandscape();
+		std::vector<double> Get_FitnessLandscape();
+
+};
+
+
+
+
+#endif
