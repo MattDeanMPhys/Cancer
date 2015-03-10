168c4609feedc5bde0d5109f5902911f0eea6118
dm13450 <deanmarkwick@Deans-MacBook-Pro.local>
2015-03-10 10:28:05 +0000

    Continued derivations and phase portraits plot

--- Fisher Waves/Phase/phasePotrait.r
+++ Fisher Waves/Phase/phasePotrait.r
@@ -1,6 +1,8 @@
 rm(list=ls())
 library(ggplot2)
 library(grid)
+library(gridExtra)
+library(tikzDevice)
 
 phaseSpace <- function(u0, v0, h, c){
 	u = c(u0)
@@ -50,21 +52,28 @@ phasePotrait2 <- function(u, v, h, c){
 }
 
 c = 3
+v = 1
 a = phasePotrait2(1, 1, 0.05, c)
-b = phaseSpace(1 ,1 , 0.05, c) 
+b = phasePotrait2(1 ,1 , 0.05, v) 
 k = 1
-while(k < 5000){
+while(k < 1000){
 
-	u0 = runif(1, 0, 1.5)
+	u0 = runif(1, -1, 1.5)
 	v0 = runif(1, -1, 1)
 
 	a = rbind(a, phasePotrait2(u0,v0, 0.05, c))
-	#b = rbind(b, phaseSpace(u0, v0, 0.05, c))
+	b = rbind(b, phasePotrait2(u0, v0, 0.05, v))
 	k = k + 1
 }
 
+grph2 = ggplot(a, aes(U, V)) + geom_point(alpha = 0.2) + geom_segment(aes(xend = dU, yend = dV  ), arrow = arrow(length = unit(0.1, "cm"))) + ggtitle("$v=3$") + theme_bw() + scale_x_continuous(expand=c(0,0))
+grph3 = ggplot(b, aes(U, V)) + geom_point(alpha = 0.2) + geom_segment(aes(xend = dU, yend = dV  ), arrow = arrow(length = unit(0.1, "cm"))) + ggtitle("$v=1$") + theme_bw() + scale_x_continuous(expand=c(0,0))
 
-grph2 = ggplot(a, aes(U, V)) + geom_point(alpha = 0.2) + geom_segment(aes(xend = dU, yend = dV, colour = length ), arrow = arrow(length = unit(0.1, "cm"))) + scale_colour_gradient(high="red")
-#grph2 = grph2 + geom_line(data = b, aes(x = U, y=V, group = label), na.rm=T)
+tikz(file = "phasePortrait1.tex", width = 3, height = 3)
+print(grph3)
+dev.off()
 
+tikz(file = "phasePortrait3.tex", width = 3, height = 3)
+print(grph2)
+dev.off()
 
