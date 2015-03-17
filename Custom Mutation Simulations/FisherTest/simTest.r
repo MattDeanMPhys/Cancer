2f7e8bcb370dc195a0456e443168108678129065
dm13450 <deanmarkwick@wh067e.halls.manchester.ac.uk>
2015-03-17 20:58:42 +0000

    Simulations and test script update

--- Custom Mutation Simulations/FisherTest/simTest.r
+++ Custom Mutation Simulations/FisherTest/simTest.r
@@ -6,18 +6,34 @@ files = list.files()
 
 statFiles = grep("Statistic", files, value = TRUE)
 
-i = 1 
+#i = 4 
+#
+#while(i <= length(statFiles)){
+#
+#	data = read.csv(statFiles[i], head = T, sep = "\t")
+#
+#	graphD = ggplot(data, aes(Time, Displacement)) + geom_line()
+#	graphVel = ggplot(data, aes(Time, Velocity)) + geom_line()
+#	graphVar =  ggplot(data, aes(Time, Variance)) + geom_line()
+#	
+#	quartz()
+#	grid.arrange(graphD, graphVel, graphVar, ncol=2)
+#
+#	i = i + 1
+#}
 
-while(i <= length(statFiles)){
+posData = read.csv("1514087213008365_StatisticOutput.txt", head = T, sep = "\t")
+negData = read.csv("1517043321160289_StatisticOutput.txt", head = T, sep = "\t")
 
-	data = read.csv(statFiles[i], head = T, sep = "\t")
+graphVelPos = ggplot(posData, aes(Time, Velocity)) + geom_line() + ggtitle("Increasing Mutation Rate, 100 Mutations")
+graphVelNeg = ggplot(negData, aes(Time, Velocity)) + geom_line() + ggtitle("Decreasing Mutation Rate, 100 Mutations")
 
-	graphD = ggplot(data, aes(Time, Displacement)) + geom_line()
-	graphVel = ggplot(data, aes(Time, Velocity)) + geom_line()
-	graphVar =  ggplot(data, aes(Time, Variance)) + geom_line()
-	
-	quartz()
-	grid.arrange(graphD, graphVel, graphVar, ncol=2)
+posData50 = read.csv("1509269749571109_StatisticOutput.txt", head = T, sep = "\t")
+negData50 = read.csv("1509518214863183_StatisticOutput.txt", head = T, sep = "\t")
 
-	i = i + 1
-}
+graphVelPos50 = ggplot(posData, aes(Time, Velocity)) + geom_line() + ggtitle("Increasing Mutation Rate, 50 Mutations")
+graphVelNeg50 = ggplot(negData, aes(Time, Velocity)) + geom_line() + ggtitle("Decreasing Mutation Rate, 50 Mutations")
+
+
+
+
